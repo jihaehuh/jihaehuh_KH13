0b0996fa6366825e1317c08c1196aa5aea4cf992
jihaehuh <jihaehuh@naver.com>
2023-12-05 12:53:40 +0900

    디지털 컨버스 추가

--- day02/src/welcome/Information.java
+++ day02/src/welcome/Information.java
@@ -3,7 +3,7 @@ package welcome;
 public class Information {
 	public static void main(String[] args) { //메인 메소드
 		System.out.println("허지혜"); //출력 명령
-		System.out.println("공공데이터 융합웹 애플리케이션 양성과정 7");
+		System.out.println("(디지털 컨버스)공공데이터 융합웹 애플리케이션 양성과정 7");
 	}
 
 }

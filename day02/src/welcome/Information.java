7de97d2e4f654aa38ce9aa6e29f86281e91c19af
jihaehuh <jihaehuh@naver.com>
2023-12-05 12:31:09 +0900

    Information 프로그램 / 이름/과정

---
+++ day02/src/welcome/Information.java
@@ -0,0 +1,9 @@
+package welcome;
+
+public class Information {
+	public static void main(String[] args) { //메인 메소드
+		System.out.println("허지혜"); //출력 명령
+		System.out.println("공공데이터 융합웹 애플리케이션 양성과정 7");
+	}
+
+}

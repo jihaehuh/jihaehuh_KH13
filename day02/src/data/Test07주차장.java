d79731e217c60ceed3d7cbd265c7fdaa786293cd
jihaehuh <jihaehuh@naver.com>
2023-12-05 18:21:05 +0900

    07 아직 못품

---
+++ day02/src/data/Test07주차장.java
@@ -0,0 +1,30 @@
+/*
+KH주차장은 10분당 1500원의 요금을 받고 있습니다.
+처음 9분까지는 돈을 받지 않고 10분이 되면 1500원, 20분이 되면 3000원으로 요금이 올라갑니다
+
+11시 50분에 들어와서 17시 25분에 나간 차량의 요금을 출력
+(단, 다음날까지 주차는 절대로 불가능합니다)
+
+*/
+package data;
+
+public class Test07주차장 {
+	public static void main(String[]args) {
+		
+		//입력 부분
+		int minute = 10;
+		int minutePrice =1500;
+		int dminute = 20; 
+		int dminutePrice = 3000;
+		
+		//계산 부분
+		
+		
+		
+		//출력 부분
+		
+		
+		
+				
+	}
+}

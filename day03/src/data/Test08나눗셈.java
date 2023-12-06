0da3673849307a664e9d4c920f45f9f2f5f36fde
jihaehuh <jihaehuh@naver.com>
2023-12-06 10:56:54 +0900

    Test08나눗셈 숫자자리

---
+++ day03/src/data/Test08나눗셈.java
@@ -0,0 +1,24 @@
+package data;
+
+public class Test08나눗셈 {
+	public static void main(String[]args) {
+		int number=75;
+		
+		//위 숫자의 10의 자리 ,1의 자리 숫자를 각각 출력
+		
+		//System.out.println(number / 10);
+		//System.out.println(number % 10);
+		
+		//숫자의 100의 자리,10의자리,1의 자리를 각각 출력
+		int number2 =139;
+		System.out.println(number2 / 100);
+		System.out.println(number2 / 10 %10);
+		System.out.println(number2 %10);
+		
+		
+		
+		
+		
+		
+	}
+}

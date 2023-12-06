b7a7e8689d0e2fa596cb28c619baad9400df67f2
jihae <jihae@zeze>
2023-12-06 23:29:56 +0900

    과제 내풀이

--- day03/src/data2/test06페이지번호계산기.java
+++ day03/src/data2/test06페이지번호계산기.java
@@ -4,28 +4,16 @@ public class test06페이지번호계산기 {
 	public static void main(String[]args) {
 		
 		//입력
-		int myPageNum =275;
-	//	271~280   (270~279)
-		int first = 1;
-		int last = 10;
-		
-		int a = (myPageNum-1) /10*10+first;
-		int b = (myPageNum-1) /10*10+last;
-		
-		System.out.println(a);
-		System.out.println(b);
-		
+		int myP =275;
+		//	271~280   (270~279)
+		int firstN = 1;
+		int lastN = 10;
 		//계산
-	
-		int firstResult =(myPageNum-1)/10*10+1;
-		System.out.println(firstResult);
-		int lastResult=(firstResult-1)/10*10+1;
-		System.out.println(lastResult);
+		int firstR = (myP-1) /10*10+firstN;
+		int lastR = (myP-1) /10*10+lastN;
 		// 출력
-		//System.out.println(resultfirstPagenum+"부터"+resultlastpagenum);
-		
-		
-		
+		System.out.println(firstR);
+		System.out.println(lastR);
 	
 		
 	}

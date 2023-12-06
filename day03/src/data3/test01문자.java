0f0148fbc3e8698b89b9b4b03d71e49060432432
jihaehuh <jihaehuh@naver.com>
2023-12-06 17:16:20 +0900

    문자 단위 이론

--- day03/src/data3/test01문자.java
+++ day03/src/data3/test01문자.java
@@ -1,5 +1,23 @@
 package data3;
 
 public class test01문자 {
-
+	public static void main(String[]args) {
+		// 자바의문자표현 방식
+		//-자바는 유니코셋을 표현할 수 있다.
+		//-char 형태로 표현(한 글자만 가능)
+		
+		
+		char a ='가';
+		System.out.println(a);
+		
+		int d =a;
+		System.out.println(d);
+		
+		char C=44032;
+		System.out.println(C);
+		
+		//'가'는 44032 '힣'은 55203
+		
+		
+	}
 }

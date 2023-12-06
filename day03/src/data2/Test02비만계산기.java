27e758c61fd54450f3eb739b289b71e0e5f11b8a
jihaehuh <jihaehuh@naver.com>
2023-12-06 14:07:06 +0900

    Test02비만계산기

---
+++ day03/src/data2/Test02비만계산기.java
@@ -0,0 +1,17 @@
+package data2;
+
+public class Test02비만계산기 {
+	public static void main(String[]args) {
+		double cm =180;
+		double kg =80;
+		double m =cm /100;
+		double bmi =kg/(m*m);
+		//double bmi =kg/m/m;
+		
+		System.out.println(bmi);
+		
+		
+		
+		
+		}
+	}

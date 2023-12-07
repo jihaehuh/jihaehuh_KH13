df08f4365b9d2225c4b2a799115f65abdfd674c2
jihaehuh <jihaehuh@naver.com>
2023-12-07 09:46:49 +0900

    문제 설명 추가

--- day03/src/data2/Test02비만계산기.java
+++ day03/src/data2/Test02비만계산기.java
@@ -1,3 +1,8 @@
+/*Test02비만계산기
+BMI(체질량지수) 계산 공식은 다음과 같습니다.
+키가 180cm, 몸무게가 80kg인 사람의 BMI 지수를 구하여 출력하세요
+*/
+
 package data2;
 
 public class Test02비만계산기 {
--- day03/src/data2/test06페이지번호계산기.java
+++ day03/src/data2/test06페이지번호계산기.java
@@ -18,3 +18,25 @@ public class test06페이지번호계산기 {
 		
 	}
 }
+
+/*package data2;
+
+public class test06페이지번호계산기 {
+	public static void main(String[]args) {
+		
+		//입력
+		int myP =275;
+		//	271~280   (270~279)
+		int firstN = 1;
+		int lastN = 10;
+		//계산
+		int firstR = (myP-1) /10*10+firstN;
+		int lastR = (myP-1) /10*10+lastN;
+		// 출력
+		System.out.println(firstR);
+		System.out.println(lastR);
+	
+		
+	}
+}
+*/

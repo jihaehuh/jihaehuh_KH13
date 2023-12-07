5be3a4dd28fb7015555996531bdb41bdc092656b
jihae <jihae@zeze>
2023-12-07 20:40:47 +0900

    과제 완성

--- day04/src/IO/Test06분식점.java
+++ day04/src/IO/Test06분식점.java
@@ -11,31 +11,34 @@ KH분식점의 메뉴판은 다음과 같습니다.
 
 package IO;
 
-import java.util.Scanner;
+import java.util.Scanner;//백준 자바 11
 
 public class Test06분식점 {
 	public static void main(String[]args) {
 		//입력 
+		int ddeokbokiPrice=3000;
+		int friedPrice =500;
+		int soondaePrice =2000;
 		//떡볶이, 튀김, 순대 개수 입력 받기 
 		Scanner sc =new Scanner(System.in);
 		
-		System.out.println("떡볶이 개수:  ");
-		int ddeokboki =sc.nextInt();
-		System.out.println("튀김:  ");
-		int fried =sc.nextInt();
-		System.out.println("순대 개수:  ");
-		int soondae =sc.nextInt();
-		
-		//백준 자바 11
-		
-		
-		
+		System.out.print("떡볶이 수량: ");
+		int ddeokbokiNum =sc.nextInt();
+		System.out.print("튀김 개수: ");
+		int friedNum =sc.nextInt();
+		System.out.print("순대 수량: ");
+		int soondaeNum =sc.nextInt();
 		
 		//계산 
 		//합계에서 10퍼센트 할인 가격
+		int sum =(ddeokbokiPrice * ddeokbokiNum) 
+				 +(friedPrice * friedNum )
+				 +( soondaePrice * soondaeNum);
+		int saleSum = sum-(sum /10);
+		
+		
 		//결제 금액 출력
-		
-		
+		System.out.print("총" + saleSum+ "원 입니다.");
 		
 		
 		

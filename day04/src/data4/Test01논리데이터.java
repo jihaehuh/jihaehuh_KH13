ac9c2fb6c7ac8eb7035f68f72ff823bb8cda77cc
jihaehuh <jihaehuh@naver.com>
2023-12-07 10:22:31 +0900

    논리데이터 설명

--- day04/src/data4/Test01논리데이터.java
+++ day04/src/data4/Test01논리데이터.java
@@ -1,5 +1,22 @@
 package data4;
 
 public class Test01논리데이터 {
-
+	public static void main(String[]args) {
+		//논리
+		//-판정을 위한 데이터
+		//-True/False 두 가지만 가질수 있다
+		//-형태종류도 boolean 하나뿐이다
+		
+		boolean a = true;
+		//boolean a= 1; 이건 안됨
+		boolean b = false;
+		System.out.println(a);
+		System.out.println(b);
+		
+		//(ex) 가진돈이 28000원이고, 피자가 20000원이면 주문이 가능한가?
+		int money =28000;
+		int pizza =20000;
+		boolean order = money >= pizza; //머니가 피자 이상이냐?
+		System.out.println("order = "+ order); 
+	}
 }

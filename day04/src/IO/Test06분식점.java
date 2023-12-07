eecb8e8c7aa8c69bc78548f8cbce33a74acd4ebe
jihaehuh <jihaehuh@naver.com>
2023-12-07 18:25:56 +0900

    과제 미완성

---
+++ day04/src/IO/Test06분식점.java
@@ -0,0 +1,43 @@
+/*
+Test06분식점
+KH분식점의 메뉴판은 다음과 같습니다.
+
+사용자에게 떡볶이, 튀김, 순대 개수를 입력받아 결제금액을 출력
+(단, 현재 행사중이라서 10% 할인된 가격으로 판매합니다) 
+떡볶이 3000원
+튀김 500원
+순대 2000원
+*/
+
+package IO;
+
+import java.util.Scanner;
+
+public class Test06분식점 {
+	public static void main(String[]args) {
+		//입력 
+		//떡볶이, 튀김, 순대 개수 입력 받기 
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("떡볶이 개수:  ");
+		int ddeokboki =sc.nextInt();
+		System.out.println("튀김:  ");
+		int fried =sc.nextInt();
+		System.out.println("순대 개수:  ");
+		int soondae =sc.nextInt();
+		
+		//백준 자바 11
+		
+		
+		
+		
+		//계산 
+		//합계에서 10퍼센트 할인 가격
+		//결제 금액 출력
+		
+		
+		
+		
+		
+	}
+}

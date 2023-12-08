ff9e87416087eff9320caa9ddcf19d11fe6c7073
jihaehuh <jihaehuh@naver.com>
2023-12-08 15:17:23 +0900

    조건 else if 설명

--- day05/src/condition2/Test01그룹조건2.java
+++ day05/src/condition2/Test01그룹조건2.java
@@ -13,8 +13,19 @@ public class Test01그룹조건2 {
 		 * [1] 버스 [2] 지하철 [3] 택시 [4] 죄송..
 		 */
 		
-		int minute =140;
-		
-		
+		int minute =20;
+		//조건의 우선순위를 잘 생각해서 맨위로 올려줘야한다.
+		if(minute>=120) {//지하철을 탈 수 있는 상황이라면
+			System.out.println("지하철을 타고 학원에 갑니다.");
+		}
+		else if(minute>=60) {//버스를 탈 수 있는 상황이라면
+			System.out.println("버스를 타고 학원에 갑니다.");
+		}
+		else if(minute >=30) {//택시를 탈 수 있는 상황이라면
+			System.out.println("택시를 타고 학원에 갑니다.");
+		}
+		else {// 마지막....전화
+			System.out.println("강사님께 전화합니다.");
+		}
 	}
 }

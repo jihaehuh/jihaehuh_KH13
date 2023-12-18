63d59f92053663c030b8065424250635cee0d9ce
jihae <jihae@zeze>
2023-12-18 22:12:23 +0900

    내 풀이

--- day11/src/oop/method7/ScorePass.java
+++ day11/src/oop/method7/ScorePass.java
@@ -1,5 +1,48 @@
 package oop.method7;
 
 public class ScorePass {
-
+	int grade;
+	int team;
+	String name;
+	int korean;
+	int english;
+	int math;
+	int total;
+	int average;
+	
+	void data(int grade,int team,String name,int korean,int english,int math,int total,int average) {
+		this.grade=grade;
+		this.team=team;
+		this.name =name;
+		this.korean =korean;
+		this.english=english;
+		this.math=math;
+		this.total=total;
+		this.average=average;
+	}
+	void information() {
+		
+		System.out.println("학년 : "+ this.grade);
+		System.out.println(" 반 : "+ this.team);
+		System.out.println("이름 : "+ this.name);
+		
+		int total = this.korean+this.english+this.math;
+		int average = total/3;
+		System.out.println("총점:"+ total +"점,"+" 평균: " + average+"점");
+		
+		if(average <= 60) {
+			System.out.println("재평가 입니다");
+		}
+		else if(this.korean <= 40 ||this.english <= 40 || this.math <= 40) {
+			System.out.println("재평가 입니다");
+		}
+		else {
+			System.out.println("통과입니다");
+		}
+		System.out.println();
+		
+	}
+	
+	
+	
 }
--- day11/src/oop/method7/Test01.java
+++ day11/src/oop/method7/Test01.java
@@ -1,5 +1,20 @@
 package oop.method7;
 
 public class Test01 {
+	public static void main(String[]args) {
+		ScorePass p1 = new ScorePass();
+		ScorePass p2 = new ScorePass();
+		ScorePass p3 = new ScorePass();
+		ScorePass p4 = new ScorePass();
 
+		p1.data(1,1,"마리오",50,60,50,160,53);
+		p2.data(1,1,"루이지",60,90,50,200,66);
+		p3.data(1,1,"쿠파",70,70,80,220,73);
+		p4.data(1,1,"요시",80,85,35,200,66);
+
+		p1.information();
+		p2.information();
+		p3.information();
+		p4.information();
+	}
 }

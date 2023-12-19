4d7d6a4d066807867daf3ccccbd11c1cfeaf7cbe
jihaehuh <jihaehuh@naver.com>
2023-12-19 10:38:04 +0900

    get메소드  + return 설명

--- day12/src/oop/method8/Student.java
+++ day12/src/oop/method8/Student.java
@@ -17,6 +17,23 @@ public class Student {
         this.math = math;
     }
     
+    //보조 계산을 수행하는 메소드 생성
+    //-**return키워드**를 사용하여 값을 반환하도록 작성해야한다
+    //-형태를 맞춰줘야함 int=>int  /if=>boolean /double=>double
+    int getTotal() {//int 가 사용결과로 남는 getTotal 메소드
+    	return this.korean +this.english +this.math;
+    }
+    
+    double getAverage() {//double 이 사용결과로 남는 getAverage 메소드
+    	return this.getAverage()/3d; 
+    }
+   
+    boolean getPass() {//boolean 이 사용결과로 남는 getPass메소드
+    	return this.korean >=40 && this.english >=40 && 
+    			this.math >=40 && this.getAverage() >=60;
+    }
+    
+    
     void information() {
         System.out.println("<학생 정보>");
         System.out.println(this.level+"학년 " +this.group+"반 "+this.name);
@@ -24,14 +41,19 @@ public class Student {
         System.out.println("영어 : " + this.english + "점");
         System.out.println("수학 : " + this.math + "점");
         
-        int total = this.korean + this.english + this.math;
-        System.out.println("총점 : " + total + "점");
-        double average = total / 3d;
-        System.out.println("평균 : " + average + "점");
+       // int total = this.korean + this.english + this.math; 
+        //위에서 계산해서 get으로 불러로오기
+        System.out.println("총점 : " + this.getTotal() + "점");
+        //double average = total / 3d;
+        //위에서 계산해서 get으로 불러오기
+        System.out.println("평균 : " + this.getAverage() + "점");
         
-        if(this.korean >= 40 && this.english >= 40 
-                        && this.math >= 40 && average >= 60) {
-            System.out.println("당신은 통과하였습니다");
+       // if(this.korean >= 40 && this.english >= 40 
+       //                 && this.math >= 40 && average >= 60) {
+       //   System.out.println("당신은 통과하였습니다");
+       // }//위에서 계산해서 get으로 불러오기
+        if(this.getPass()/*==true*/) {
+        	System.out.println("당신은 통과하였습니다");
         }
         else {
             System.out.println("당신은 재평가 대상자입니다");
---
+++ day12/src/oop/method8/Test01.java
@@ -0,0 +1,11 @@
+package oop.method8;
+
+public class Test01 {
+	public static void main(String[]args) {
+		Student a = new Student();
+		a.data(1, 1, "마리오", 60,70,50);
+	}
+
+		
+	}
+

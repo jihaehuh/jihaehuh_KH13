3bf5b15ab49ab17d6ed478313464f8f416bc217a
jihae <jihae@zeze>
2023-12-19 23:50:04 +0900

    내 풀이

--- day12/src/loop/constructor2/Music.java
+++ day12/src/loop/constructor2/Music.java
@@ -15,22 +15,52 @@ public class Music {
 		this.singer = singer;
 	}
 	void setPlay(double play) {
+		if(play>=0) { 
+		}
 		this.play = play;
 	}
 	
 	void setLikes(double likes) {
+		if(likes>=0) {
+		}
 		this.likes = likes;
 	}
-	
-	Music(String title, String singer,double play,double likes){
+	double getBest(){
+		if(this.play >100000){//재생수가 10만이 넘을 때
+		}
+		else {
+		
+	}
+		return this.getBest();
+	}
+	double getPopular() {//좋아요수가 10만이 넘을 때
+		return this.getPopular();
+	}
+	//랭킹 점수 = 재생수 * 2 + 좋아요수 / 2
+	double getRank() {
+		return this.play *2 + this.likes /2;
+	}
+		
+		Music(String title, String singer,double play,double likes){
 		this.setTitle(title);
 		this.setSinger(singer);
 		this.setPlay(play);
 		this.setLikes(likes);
 	}
-	void information() {
+	
+		void information() {
 		System.out.println("<음원 차트>");
-		System.out.println("가수 : " +this.singer);
+		System.out.println("제목 : " +this.title);
+		if(this.play>100000) {
+			System.out.print("(인기곡)");
+		}
+		if(this.likes>100000) {
+			System.out.print("(베스트)");
+		}
+		System.out.println("가수 : "+ this.singer);
+		System.out.println("재생수 : "+this.play);
+		System.out.println("좋아요수 : "+this.likes);
+		System.out.println("랭킹점수 : "+this.getRank());
 	}
 	
 	
--- day12/src/loop/constructor2/Test01생성자.java
+++ day12/src/loop/constructor2/Test01생성자.java
@@ -1,5 +1,16 @@
 package loop.constructor2;
 
 public class Test01생성자 {
-
+	public static void main(String[]args) {
+		Music a = new Music("perfect Night","르세라핌",104250d,91835d);
+		Music b = new Music("첫 눈","EXO",83127d,90805d);
+		Music c = new Music("Drama","aespa",64590d,174519d);
+		Music d = new Music("To.X","태연",58826d,70313d);
+		
+		a.information();
+		b.information();
+		c.information();
+		d.information();
+		
+	}
 }

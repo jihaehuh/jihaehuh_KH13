6730400a05981eb9ad02567af4425226dafc2d9a
jihaehuh <jihaehuh@naver.com>
2023-12-22 14:04:33 +0900

    업캐스팅을 해야 다운캐스팅도 가능함

---
+++ day15/src/oop/poly3/Teacher.java
@@ -0,0 +1,6 @@
+package oop.poly3;
+
+public abstract class Teacher {
+	public abstract void teach();
+	
+	}
---
+++ day15/src/oop/poly3/Test01캐스팅.java
@@ -0,0 +1,30 @@
+package oop.poly3;
+
+public class Test01캐스팅 {
+	public static void main(String[] args) {
+		//Hwang 의 객체를 만들어 업/다운 캐스팅 구현
+		
+		
+		//집
+		Hwang h1 = new Hwang();
+		h1.game();
+		h1.drink();
+		h1.teach();
+		
+		System.out.println("------------");
+		
+		//출근 
+		Teacher t= h1; //업캐스팅 (Hwang->Teacher)
+		//t.game();//안보이는 기능
+		//t.drink();//안보이는 기능
+		t.teach();//재정의 되어 있어서 연결 되어 실행
+		
+		
+		//퇴근
+		//Hwang h2 =t; //다운캐스팅 (Teacher -> Hwang)
+		Hwang h2 =(Hwang)t; //다운캐스팅 (Teacher -> Hwang)
+		h2.game();
+		h2.drink();
+		h2.teach();
+	}
+}

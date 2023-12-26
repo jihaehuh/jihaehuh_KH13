be743748dc4b4eb5241e6c0974c2b2213e7a1751
jihaehuh <jihaehuh@naver.com>
2023-12-26 18:20:13 +0900

    과제 미완성

---
+++ day16/src/api/lang/string/Test06공포의쿵쿵따.java
@@ -0,0 +1,81 @@
+package api.lang.string;
+
+import java.util.Scanner;
+
+public class Test06공포의쿵쿵따 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("<공포의 쿵쿵따 게임 >");
+		System.out.println("바나나! 쿵쿵따");
+		String user= sc .next();
+		
+	
+		boolean isLengthOk = user.length() ==3;
+		System.out.print("쿵쿵따");
+		
+
+		
+		
+		
+		
+
+	}
+}
+
+
+/*
+ * 
+ * 
+concat(String str)
+지정된 문자열을 이 문자열의 끝에 연결합니다.
+boolean
+contains(CharSequence s)
+이 문자열에 지정된 char 값 시퀀스가 ​​포함되어 있는 경우에만 true를 반환합니다.
+		}boolean
+		endsWith(String suffix)
+		이 문자열이 지정된 접미사로 끝나는지 테스트합니다.
+		boolean
+		endsWith(String suffix)
+		이 문자열이 지정된 접미사로 끝나는지 테스트합니다.
+import java.util.Scanner;
+
+public class Test06 {
+    public static void main(String[] args) {
+        // 초기 제시어 설정
+        String currentWord = "바나나";
+
+        // Scanner 객체를 사용하여 사용자로부터 단어 입력 받기
+        Scanner scanner = new Scanner(System.in);
+
+        // 쿵쿵따 게임 시작
+        while (true) {
+            // 현재 제시어 출력
+            System.out.println(currentWord + "! 쿵쿵따!");
+
+            // 사용자에게 단어 입력 받기
+            System.out.print("(" + currentWord.substring(currentWord.length() - 3) + ")");
+            String userInput = scanner.nextLine();
+
+            // 입력한 단어가 3글자가 아니면 게임 오버
+            if (userInput.length() != 3) {
+                System.out.println("땡! 게임오버!");
+                break;
+            }
+
+            // 입력한 단어와 제시어의 마지막 3글자가 일치하면 제시어 변경
+            if (userInput.equals(currentWord.substring(currentWord.length() - 3))) {
+                currentWord = userInput;
+            } else {
+                // 일치하지 않으면 게임 오버
+                System.out.println("땡! 게임오버!");
+                break;
+            }
+        }
+
+        // 게임 종료
+        scanner.close();
+    }
+}
+
+*/

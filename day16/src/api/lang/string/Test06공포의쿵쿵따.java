7b64b29161215c2b4a45ff42c513b3c68fc01431
jihaehuh <jihaehuh@naver.com>
2023-12-27 10:28:29 +0900

    쿵쿵따 강사님 풀이

--- day16/src/api/lang/string/Test06공포의쿵쿵따.java
+++ day16/src/api/lang/string/Test06공포의쿵쿵따.java
@@ -1,9 +1,10 @@
 
-
+/*
 package api.lang.string;
 
 import java.util.Scanner;
 
+//369 게임이랑 7이 들어가는 숫자 예제랑 비슷한 문제
 public class Test06공포의쿵쿵따 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -31,97 +32,125 @@ public class Test06공포의쿵쿵따 {
         }
 	}
 
+*/
 
+ 
 
-
-/*
- * 
- * 
-concat(String str)
-지정된 문자열을 이 문자열의 끝에 연결합니다.
-boolean
-contains(CharSequence s)
-이 문자열에 지정된 char 값 시퀀스가 ​​포함되어 있는 경우에만 true를 반환합니다.
-		}boolean
-		endsWith(String suffix)
-		이 문자열이 지정된 접미사로 끝나는지 테스트합니다.
-		boolean
-		endsWith(String suffix)
-		이 문자열이 지정된 접미사로 끝나는지 테스트합니다.
-		
 		
 package api.lang.string;
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Test06공포의쿵쿵따 {
     public static void main(String[] args) {
-        // 초기 제시어 설정
-        String currentWord = "바나나";
-
-        // Scanner 객체를 사용하여 사용자로부터 단어 입력 받기
-        Scanner scanner = new Scanner(System.in);
-
-        // 쿵쿵따 게임 시작
-        while (true) {
-            // 현재 제시어 출력
-            System.out.println(currentWord + "! 쿵쿵따!");
-
-            // 사용자에게 단어 입력 받기
-            System.out.print("(" + currentWord.substring(currentWord.length() - 3) + ")");
-            String userInput = scanner.nextLine();
-
-            // 입력한 단어가 3글자가 아니면 게임 오버
-            if (userInput.length() != 3) {
-                System.out.println("땡! 게임오버!");
-                break;
-            }
-
-            // 입력한 단어와 제시어의 마지막 3글자가 일치하면 제시어 변경
-            if (userInput.equals(currentWord.substring(currentWord.length() - 3))) {
-                currentWord = userInput;
-            } else {
-                // 일치하지 않으면 게임 오버
-                System.out.println("땡! 게임오버!");
-                break;
-            }
-        }
-
-        // 게임 종료
-        scanner.close();
+     //(Q)사용자가 입력한 단어가 "바나나"와 이어지는지 검사 
+    	//1. 사용자가 입력한 단어가 "나"로 시작하는지 검사
+    	//3 .입력 추가
+    	Scanner sc = new Scanner(System.in);
+        //6. 랜덤 추가 
+    	//String given ="바나나";
+    	String[]words = new String[] {
+    			"바나나","물고기","초콜릿","이발소","컴퓨터"
+    	};
+    	
+    	Random r = new Random();
+    	
+    	int position =r.nextInt(words.length);
+    	String given = words[position];
+    	
+    	int round=1; //1라운드 시작
+    	
+    	
+    	//5.반복추가
+    	while (true) {
+        
+        	
+       // String given ="바나나"; //밖으로 빼고 누적 계산해줘야함
+        System.out.println(given + "! 쿵쿵따!");
+        //String input ="나트륨";
+        String input =sc.next();   
+        
+        /*2.given 의 마지막 글자로 input이 시작
+    	given 의 마지막 글자 == input의 시작글자 */
+    	//char last = given.charAt(2);//+2지점 글자
+    	char last = given.charAt(given.length()-1); //마지막 글자
+    	char first = input.charAt(0); 
+    	//System.out.println(first==last);  //원시형이라서 비교연산으로 비교
+    	//System.out.println(input.startsWith("나"));
+    	//System.out.println(input.charAt(0) == '나');
+    	
+    	
+    	//4. 판정 추가 
+    	boolean isLengthOk = input.length() == 3;
+    	boolean isConnect =first ==last;
+    	
+    	//System.out.println("글자수 = "+ isLengthOk);
+    	//System.out.println("연결 = "+ isConnect);
+    	
+    	boolean isOk = isLengthOk && isConnect;
+    	if(isOk) {//제시어를 입력 값으로 변경
+    		given = input;
+    		round++; //맞추면 라운드 증가
+    	}
+    	else { //입력이 잘못되었다면
+    		break;
+    	}
+    	System.out.println("게임 끝 !");
+    	System.out.println("총" + round +"라운드까지 진행");
+    	
+    	}
+    	
+    	
     }
+    
 }
 
 
 /*
+ * package api.lang.string;
 
-package api.lang.string;
-
+import java.util.Random;
 import java.util.Scanner;
 
-public class Test06공포의쿵쿵따 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Test05공포의쿵쿵따8 {
+	public static void main(String[] args) {
+		//(Q) 업그레이드(제시어랜덤, 라운드추가)
+		Scanner sc = new Scanner(System.in);
 
-        String firstWord = "바나나";
-        boolean isCorrect = true;
+		String[] words = new String[] {
+			"바나나", "물고기", "초콜릿", "이발소", "컴퓨터"
+		};
+		Random r = new Random();
 
-        System.out.println(firstWord + "! 쿵쿵따!");
+		//String given = "바나나";
+		int position = r.nextInt(words.length);
+		String given = words[position];
+		int round = 1;//1라운드부터 시작
+		while(true) {
+			System.out.print(given+"! 쿵쿵따! ");
+			String input = sc.next();
+			char last = given.charAt(given.length()-1);
+			char first = input.charAt(0);
 
-        while (isCorrect) {
-            String user = sc.next();
+			//판정계산
+			boolean isLengthOk = input.length() == 3;
+			boolean isConnect = first == last;
+			boolean isOk = isLengthOk && isConnect;
+			if(isOk) {
+				//제시어를 입력값으로 변경
+				given = input;
+				round++;//맞추면 라운드 증가
+			}
+			else {//입력이 잘못되었다면
+				break;
+			}
+		}
 
-            if (user.length() != 3) {
-                System.out.println("땡! 게임오버!");
-                isCorrect = false;
-            } else if (user.equals(firstWord.substring(firstWord.length() - 3))) {
-                firstWord = user;
-                System.out.println(firstWord + "! 쿵쿵따!");
-            } else {
-                System.out.println("땡! 게임오버!");
-                isCorrect = false;
-            }
-        }
-    }
+		//게임오버처리
+		System.out.println("게임 끝!");
+		System.out.println("총 "+round+"라운드까지 진행");
+
+	}
 }
-
 */

acab2efbd9158d84086e5d9a11bc94a3f2eab689
jihaehuh <jihaehuh@naver.com>
2024-01-03 10:36:00 +0900

    Treeset과 Hashset차이

---
+++ day21/src/api/util/collection2/Test01Set2.java
@@ -0,0 +1,39 @@
+package api.util.collection2;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test01Set2 {
+	public static void main(String[] args) {
+		//Set<E>
+		//-전체를 매우 빠르게 탐색할 수 있도록 신경쓴 구조
+		//-구조에 따라 TreeSet,HashSet등이 존재
+		
+		//TreeSet<String>ts = new TreeSet<>();
+		Set<String> hs = new HashSet<>(); //업캐스팅
+		
+		//데이터 추가
+		hs.add("피카츄");
+		hs.add("라이츄");
+		hs.add("파이리");
+		hs.add("꼬부기");
+		
+		hs.add("피카츄");
+		hs.add("피카츄");
+		hs.add("피카츄");
+		
+		//데이터가 중복이 허용되지 않는다
+		
+		//데이터 검색
+		//-피카츄가 저장소에 있습니까?
+		System.out.println(hs.contains("피카츄"));
+		
+		//데이터삭제
+		hs.remove("파이리");
+		
+		//데이터 출력
+		System.out.println(hs);
+		System.out.println(hs.size()); //개수
+	}
+}

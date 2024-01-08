af731d8b01fc999f01fe52c80ebde49b9a197270
jihaehuh <jihaehuh@naver.com>
2024-01-08 18:16:09 +0900

    emp등록

---
+++ jdbc01/src/jdbc/connect/Test02Book등록.java
@@ -0,0 +1,25 @@
+package jdbc.connect;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class Test02Book등록 {
+	public static void main(String[] args) {
+		//[1]연결 정보 생성
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("kh13"); //아이디
+		dataSource.setPassword("kh13");  //비밀번호
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");  //DB종류 지정
+		dataSource.setUrl("jdbc:Oracle:thin:@localhost:1521:xe");
+		
+		String sql ="insert into book("
+						+"book_id, book_title, book_author, book_publication_date, "
+						+ "book_price, book_publisher, book_page_count, book_genre"
+						+")"
+						+ "values(50,'자바신공','열혈강사','2023-10-01',5,'wikidocs',199,'Education')";
+	
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.update(sql);
+		System.out.println("완료");
+	}
+}
---
+++ jdbc01/src/jdbc/connect/Test04Emp등록.java
@@ -0,0 +1,47 @@
+package jdbc.connect;
+
+import java.util.Scanner;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class Test04Emp등록 {
+	public static void main(String[] args) {
+		
+		//입력
+				Scanner sc = new Scanner(System.in);
+				System.out.print("사원명 : ");
+				String empName = sc.nextLine();
+				System.out.print("부서명 : ");
+				String empDept = sc.nextLine();
+				System.out.print("입사일 : ");
+				String empDate = sc.nextLine();
+				System.out.print("급여액 : ");
+				int empSal = sc.nextInt();
+				sc.close();
+		/*
+		String empName ="ㄴㅇ혜";
+		String empDept ="KH아카데미";
+		String empDate ="2024-01-01";
+		double empSal = 90000000;
+		*/
+		//처리 JDBC
+		String sql ="insert into emp("
+				+ "emp_no,emp_name,emp_dept,emp_date,emp_sal"
+				+")"
+				+"values(emp_seq.nextval,?,?,?,?)"; //테이블에서 자동적으로 받아 와야기때문에 
+		Object[]data = {empName,empDept,empDate,empSal};
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("kh13");
+		dataSource.setPassword("kh13");
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		jdbcTemplate.update(sql,data);
+		System.out.println("실행완료");
+	}
+}
+

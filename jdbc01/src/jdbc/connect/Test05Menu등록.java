4c65a425a8a2738b2c6d7cecce222e238cda7c89
jihaehuh <jihaehuh@naver.com>
2024-01-09 09:58:34 +0900

    과제풀이 추가

--- jdbc01/src/jdbc/connect/Test05Menu등록.java
+++ jdbc01/src/jdbc/connect/Test05Menu등록.java
@@ -20,11 +20,20 @@ public class Test05Menu등록 {
 		System.out.print("메뉴 판매가(원) : ");
 		int menuPrice= sc.nextInt();
 		sc.close();
+		//입력
+		/*
+		//입력
+		String menuNameKor = "아메리카노";
+		String menuNameEng = "Americano";
+		String menuType = "커피";
+		int menuPrice = 2500;
+		
+		*/
 		
 		String sql ="insert into menu2("
-				+"menu_no,menu_name_kor,menu_name_eng, menu_type,menu_price"
-				+")"
-				+"values(menu2_seq.nextval,?,?,?,?)";
+						+"menu_no,menu_name_kor,menu_name_eng, menu_type,menu_price"
+						+")"
+						+"values(menu2_seq.nextval,?,?,?,?)";
 		
 				Object[]data = {menuNameKor,menuNameEng,menuType,menuPrice};
 				DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -33,6 +42,8 @@ public class Test05Menu등록 {
 				dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
 				dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
 				
+				//JdbcTemplate jdbcTemplate = new JdbcTemplate();
+				// jdbcTemplate.setDataSource(dataSource);
 				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 				
 				jdbcTemplate.update(sql,data);

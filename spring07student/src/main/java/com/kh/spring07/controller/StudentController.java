7c1fb1d79cd04d4c0a8ac6fc34693d18f402d22f
jihaehuh <jihaehuh@naver.com>
2024-01-16 18:30:49 +0900

    집가야쥥

--- spring07student/src/main/java/com/kh/spring07/controller/StudentController.java
+++ spring07student/src/main/java/com/kh/spring07/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.kh.spring07.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring07.dao.StudentDao;
@@ -20,4 +23,61 @@ public class StudentController {
 			return "학생 등록 완료";
 		}
 		//http://localhost:8080/student/add?name=주지훈&koreanScore=89&mathScore=97&englishScore=96
+		
+		//수정
+		@RequestMapping("/edit")
+		public String edit(@ModelAttribute StudentDto dto) {
+			if(dao.update(dto)){
+				return "학생 정보 수정 완료";
+			}
+			else {
+				return "존재하지 않는 학생 정보";
+			}
+		}
+		//http://localhost:8080/student/edit?studentId=3&name=주지윤&koreanScore=89&mathScore=97&englishScore=96
+		
+		//삭제
+		@RequestMapping("/delete")
+		public String delete(@RequestParam int studentId) {
+			if(dao.delete(studentId)) {
+				return "학생 정보 삭제 완료";
+				
+			}
+			else {
+				return "존재하지 않는 학생 정보";
+			}
+		}
+		//http://localhost:8080/student/delete?studentId=3
+		//단순 목록 조회
+		@RequestMapping("/list")
+		public String list(
+				@RequestParam(required = false ) String column,
+				@RequestParam(required = false ) String keyword) {
+			boolean isSearch = column !=null && keyword !=null;
+			List<StudentDto>list;
+			if(isSearch) {
+				list = dao.selectList(column, keyword);
+			}
+			else {
+				list = dao.selectList();
+			}
+			StringBuffer buffer = new StringBuffer();
+			for(StudentDto dto : list) {
+				buffer.append(dto.getStudentId());
+				buffer.append(",");
+				buffer.append(dto.getName());
+				buffer.append(",");
+				buffer.append(dto.getKoreanScore());
+				buffer.append(",");
+				buffer.append(dto.getMathScore());
+				buffer.append(",");
+				buffer.append(dto.getEnglishScore());
+				buffer.append("<br>");				
+			}
+			return buffer.toString();
+		}
+		//http://localhost:8080/student/list?column=name&keyword=한글
+		//한항목 조회
+		
+
 }
--- spring07student/src/main/java/com/kh/spring07/dao/StudentDao.java
+++ spring07student/src/main/java/com/kh/spring07/dao/StudentDao.java
@@ -1,8 +1,11 @@
 package com.kh.spring07.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
 
 import com.kh.spring07.dto.StudentDto;
 import com.kh.spring07.mapper.StudentMapper;
@@ -16,6 +19,7 @@ public class StudentDao {
 	private StudentMapper mapper;
 	
 	public void insert(StudentDto dto) {
+		//정보 등록
 		String sql ="insert into student( "
 				+"student_id, name, korean_score, math_score, english_score"
 				+") "
@@ -25,4 +29,40 @@ public class StudentDao {
 		};
 		jdbcTemplate.update(sql,data);
 	}
+	//정보 수정
+	public boolean update(StudentDto dto) {
+		String sql = "update student "
+				+ "set name=?, korean_score=?, math_score=?, english_score=? "
+				+ "where student_id=?";
+		Object[]data = {
+				dto.getName(), dto.getEnglishScore(), dto.getKoreanScore(), 
+				dto.getMathScore(), dto.getStudentId()
+		};
+		return jdbcTemplate.update(sql,data)>0;
+	}
+	//정보삭제
+	public boolean delete(int studentId) {
+		String sql ="delete student where student_id=?";
+		Object[]data = {studentId};
+		return jdbcTemplate.update(sql,data)>0;
+	}
+	//항목 +키워드 검색
+	public List<StudentDto> selectList(){
+		String sql ="select * from student order by student_id asc";
+		return jdbcTemplate.query(sql, mapper);
+	}
+	public List<StudentDto> selectList(String column, String keyword) {
+		String sql = "select * from student "
+						+ "where instr("+column+", ?) > 0 "
+						+ "order by "+column+" asc, student_id asc";
+		Object[] data = {keyword};
+		return jdbcTemplate.query(sql, mapper, data);
+	}
+	//한 항목 조회(번호)
+		
+	
+	
+	
+	
+	
 }

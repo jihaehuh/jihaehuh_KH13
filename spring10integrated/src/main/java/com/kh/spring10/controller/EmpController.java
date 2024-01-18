2e8fe5277fc43065992dd99ff4961139fa0e1963
jihaehuh <jihaehuh@naver.com>
2024-01-18 13:34:53 +0900

    emp추가 설명

--- spring10integrated/src/main/java/com/kh/spring10/controller/EmpController.java
+++ spring10integrated/src/main/java/com/kh/spring10/controller/EmpController.java
@@ -3,7 +3,9 @@ package com.kh.spring10.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.spring10.dao.EmpDao;
@@ -15,11 +17,13 @@ public class EmpController {
 	@Autowired
 	private EmpDao dao;
 	
-	@RequestMapping("/insert1")
+	//@RequestMapping("/insert")
+	@GetMapping("/insert") //Get방식 -일반적인 주소를 이용한 접근
 	public String insert1() {
 	return"/WEB-INF/views/emp/insert1.jsp";
 	}
-	@RequestMapping("/insert2")
+	//@RequestMapping("/insert")
+	@PostMapping("/insert") //Post방식 - Form을 이용한 데이터 전송 접근
 	public String insert2(@ModelAttribute EmpDto dto) {
 		dao.insert(dto);
 		// return "redirect:/emp/insert3"; //절대
@@ -30,4 +34,5 @@ public class EmpController {
 	public String insert3() {
 		return "/WEB-INF/views/emp/insert3.jsp";
 	}
+	
 }
--- spring10integrated/src/main/webapp/WEB-INF/views/emp/insert1.jsp
+++ spring10integrated/src/main/webapp/WEB-INF/views/emp/insert1.jsp
@@ -4,11 +4,11 @@
 <%--절대 경로 
 		<form action ="/emp/insert2"></form>
  --%>
-<form action="insert2">
-	
-	사원이름: <input name="empName"><br><br>
-	사원소속:<input name="empDept"><br><br>
-	입사날짜:<input name="empDate"><br><br>
-	사원연봉:<input name ="empSal"><br><br>
+<%-- <form action="insert2"> --%>
+	<form action ="insert" method ="post">
+	사원이름: <input name="empName" type="text" ><br><br>
+	사원소속:<input name="empDept" type="text"><br><br>
+	입사날짜:<input name="empDate" type="date"><br><br>
+	사원연봉:<input name ="empSal" type="text"><br><br>
 	<button>등록</button>
 </form>

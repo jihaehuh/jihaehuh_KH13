4bac7fb89fd3f5e55464e6fdc34ea1e983eea1d0
skdiatod <skdiatod@naver.com>
2024-01-25 14:57:25 +0900

    [1]차 mypage 비회원일때 막기

--- spring10integrated/src/main/java/com/kh/spring10/configuration/InterceptorConfiguration.java
+++ spring10integrated/src/main/java/com/kh/spring10/configuration/InterceptorConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.kh.spring10.interceptor.MemberInterceptor;
 import com.kh.spring10.interceptor.TestInterceptor;
 
 //application.properties에서 하기 힘든 설정들을 이곳에서 구현할 수 있다
@@ -16,15 +17,23 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
 		@Autowired
 		private TestInterceptor testInterceptor;
 	
+		@Autowired 
+		private MemberInterceptor memberInterceptor;
+	
+		@Override
+		public void addInterceptors(InterceptorRegistry registry) {
+			//registry에다가 인터셉터를 주소와 함께 등록
+			//registry.addInterceptor(인터셉터객체).addPathPatterns(주소);  "/**"의미는 모든 주소 
+			//registry.addInterceptor(testInterceptor)
+			//.addPathPatterns("/**");  
+			//Interceptor 에서 false로 반환해놓고 포켓몬 목록과 사원목록주소만 써놓으면 두곳은 차단할수있음
+			
+			registry.addInterceptor(memberInterceptor)
+						.addPathPatterns("/member/mypage");
 	
 	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		//registry에다가 인터셉터를 주소와 함께 등록
-		//registry.addInterceptor(인터셉터객체).addPathPatterns(주소);  "/**"의미는 모든 주소 
-		//registry.addInterceptor(testInterceptor)
-		//.addPathPatterns("/**");  
-		//Interceptor 에서 false로 반환해놓고 포켓몬 목록과 사원목록주소만 써놓으면 두곳은 차단할수있음
+	
+	
 	}
 
 }
---
+++ spring10integrated/src/main/java/com/kh/spring10/interceptor/MemberInterceptor.java
@@ -0,0 +1,32 @@
+package com.kh.spring10.interceptor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+//회원 페이지에 대한 접근 허용및 차단을 판정하여 처리하는 도구
+@Service
+public class MemberInterceptor implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {//컨트롤러가 아니라 모든 도구를 수동으로 얻어야함
+		//인터셉터는 주어진 메소드 내에서 원하는 도구를 꺼내서 사용한다
+		//-request 는 사용자의 요청 정보가 담긴 객체 (HttpSession이 있음)
+		//-response는 사용자에게 내보낼 정보가 담긴 객체
+		//-handler는 실행될 예정인 컨트롤러 매핑정보가 담긴 객체
+		
+		HttpSession session = request.getSession();  //세션객체를 꺼내고
+		
+		String loginId =(String)session.getAttribute("loginId"); //사용자 ID 추출
+		
+		if(loginId != null) {//회원이면  //정보를 어디서 받아와? 세션
+			return true;
+		}
+		else {//회원이 아니면
+			return false;
+		}
+	}
+}

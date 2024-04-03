230567504072ea988812c5e6add6dc89fe1e1ce8
jihaehuh <jihaehuh@naver.com>
2024-04-03 10:59:42 +0900

    이메일 템플릿 수정

--- spring12email/src/test/java/com/kh/spring12/MimeMessageTest03.java
+++ spring12email/src/test/java/com/kh/spring12/MimeMessageTest03.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
-public class MimeMessageTest02 {
+public class MimeMessageTest03 {
 
 	@Autowired
 	private JavaMailSender sender;
@@ -34,7 +37,7 @@ public class MimeMessageTest02 {
 		//도우미 객체 생성- 파일 전송은 안하고  UTF-8로 변환해서 전송하겠다 
 		MimeMessageHelper helper = new MimeMessageHelper(message,false,"UTF-8");  
 		//booleanMartipart, String encoding 있는 거 선택하기
-		//- boolean 을 쓰는 이유: 첨부파일 여부 및 선송 여부 확인 , encoding쓰는 이유: 글자 깨지지 않게하기 위해
+		//- boolean 을 쓰는 이유: 첨부파일 여부, encoding쓰는 이유: 글자 깨지지 않게하기 위해
 	
 		
 		//헬퍼에 정보를 설정
@@ -44,7 +47,7 @@ public class MimeMessageTest02 {
 		//내용을 외부 파일에서 불러와 전송하도록 구현 
 		//파일 찾기 
 		//- src 가 왜 4개? ---java,resources,test = classpath , src --filepath(기본값) 
-		ClassPathResource resource = new ClassPathResource("templates/email-template.html");
+		ClassPathResource resource = new ClassPathResource("templates/email-template2.html");
 		File target = resource.getFile(); //파일 추출
 		
 		StringBuffer buffer = new StringBuffer(); //문자열 합성 도구 생성
@@ -54,7 +57,17 @@ public class MimeMessageTest02 {
 		}
 		sc.close();
 		
-		helper.setText(buffer.toString(),true); //읽은 내용을 전송
+		//읽은 내용을 HTML로 해석해서 필요한 정보를 교체한 뒤 전송
+		//- Jsoup 필요 (썸머노트) - pom.xml에 Html읽는 도구 
+		
+		Document document = Jsoup.parse(buffer.toString()); //HTML로 해석
+		Element receiver = document.getElementById("receiver"); //#receiver 탐색
+		receiver.text("허지혜님");
+		
+		Element link = document.getElementById("move-link"); //#move-link 탐색
+		link.attr("href", "https://www.google.com");
+		
+		helper.setText(document.toString(),true); //변환된 내용을 전송
 		//전송
 		sender.send(message);
 	}

612823957c22333388feef4419932a7212fc5e1e
jihaehuh <jihaehuh@naver.com>
2024-05-03 18:20:17 +0900

    zz

--- spring19rest/src/main/java/com/kh/spring19/restcontroller/PocketmonRestcontroller.java
+++ spring19rest/src/main/java/com/kh/spring19/restcontroller/PocketmonRestcontroller.java
@@ -2,19 +2,17 @@ package com.kh.spring19.restcontroller;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring19.dao.PocketmonDao;
